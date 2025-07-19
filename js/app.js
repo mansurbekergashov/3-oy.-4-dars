a5ee7eecc014df2861aa09561815b0b9e5901c0f
mansurbek <ergashovmansurbek5577@gmail.com>
2025-07-19 12:59:01 +0500

    while lesson

---
+++ js/app.js
@@ -0,0 +1,122 @@
+// 2.1    Math.floor, Math.ceil, Math.round yordamida yaxlitlash
+
+
+let num = Number(prompt("Son kiriting: "));
+
+console.log("Sonning o'zi: ",num);
+console.log("Floor: ", Math.floor(num));
+console.log("Ceil: ", Math.ceil(num));
+console.log("Round: ", Math.round(num));
+
+
+
+//2.2   0 va 10 orasida random son yasab foydalanuvchiga korsatish
+
+let result = Math.floor(Math.random()*10)+1;
+
+console.log("0 dan 10 gacha random natija: ", result);
+
+
+// 2.4   while yordamida 10 ta 0 va 100 oralig'ida son chiqarish
+
+
+let min=10;
+let max=100
+let a=0;
+while (a<10){
+    let son = Math.floor(Math.random()*(max-min+1))+min;
+    console.log("10 va 100 oralig'idagi random natija: ", son);
+
+    a+=1;
+}
+
+// 2.5   kvadratga oshirish
+
+let number1 = Number(prompt("Son kiriting: "));
+console.log(Math.pow(number1, 2));
+
+
+// 2.6   1 dan 20 gacha toq sonlarni chiqarish
+
+
+let s=1
+while(s<20){
+    console.log(s);
+    s+=1
+
+}
+
+
+// 2.7    to'g'ri sonni topmaguncha do while islatish
+
+
+let randSon = Math.floor(Math.random() * 10) + 1;
+let inputSon;
+
+do {
+  inputSon = Number(prompt("1 dan 10 gacha son kiriting:"));
+
+  if (inputSon !== randSon) {
+    alert("Xato! Yana kiriting.");
+  }
+
+} while (inputSon !== randSon){
+
+alert("To‘g‘ri topdingiz!");
+}
+
+
+
+// 2.8     while yordamida 1 dan 50 gacha 3 ga bo'linadigan sonlarni chiqarish, 21 ga teng bo'lsa to'xtatish
+
+
+let raqam = 0;
+
+while (raqam<50){
+    raqam+=1;
+
+    if (raqam==21 ){
+        break;
+    }
+    else if(raqam%3==0){
+        console.log(raqam);
+    }
+
+}
+
+
+// 2.9    5 ta random son yasash va eng kattasini konsolga chiqarish
+
+let num1 = Math.floor(Math.random() * 100);
+let num2 = Math.floor(Math.random() * 100);
+let num3 = Math.floor(Math.random() * 100);
+let num4 = Math.floor(Math.random() * 100);
+let num5 = Math.floor(Math.random() * 100);
+
+console.log("Sonlar: ", num1, num2, num3, num4, num5);
+
+
+
+if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5) {
+  console.log("Eng katta son: ", num1);
+}
+else if (num2 > num1 && num2 > num3 && num3 > num4 && num2 > num5) {
+  console.log("Eng katta son: ", num2);
+}
+else if (num3 > num1 && num3 > num2 && num3 > num4 && num3 > num5) {
+  console.log("Eng katta son: ", num3);
+}
+else if (num4 > num1 && num4 > num2 && num4 > num3 && num4 > num5) {
+  console.log("Eng katta son: ", num4);
+}
+else if (num5 > num1 && num5 > num2 && num5 > num3 && num5 > num4) {
+  console.log("Eng katta son: ", num5);
+}
+else{
+  console.log("!");
+  
+}
+
+
+
+
